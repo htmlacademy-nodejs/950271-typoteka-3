fc176ecacb3504e0fa6fe0e78a9830b3a00b112e
Home <gurovg@gmail.com>
2021-03-07 21:03:38 +0300

    добавляет оставшиеся пути

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -2,11 +2,16 @@
 
 const {Router} = require(`express`);
 const category = require(`../api/category`);
+const search = require(`../api/search`);
+const article = require(`./article`);
 
 const getMockData = require(`../lib/get-mock-data`);
 
 const {
   CategoryService,
+  SearchService,
+  ArticleService,
+  CommentService,
 } = require(`../data-service`);
 
 const app = new Router();
@@ -15,6 +20,8 @@ const app = new Router();
   const mockData = await getMockData();
 
   category(app, new CategoryService(mockData));
+  search(app, new SearchService(mockData));
+  article(app, new ArticleService(mockData), new CommentService());
 })();
 
 module.exports = app;
